0a06f5fcfe91c885fbb08d42e8bd8216048f4e12
Dawn <xuedonglin1994@gmail.com>
2020-02-13 17:24:56 +0800

    235. 二叉搜索树的最近公共祖先 方法二 https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tr

--- 235_lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235_lowest-common-ancestor-of-a-binary-search-tree.py
@@ -10,11 +10,7 @@ class Solution:
         """ 
         二叉搜索树中左子树小于根节点，右子树大于根节点，
         所以可以根据节点的值来判断公共祖先
-        """
-        # 根为空或一个节点等于根
-        if not root or root == p or root == q:
-            return root
-        
+        """    
         # p q 节点的值都小于根节点
         if root.val > p.val and root.val > q.val:
             return self.lowestCommonAncestor(root.left, p, q)
@@ -22,4 +18,18 @@ class Solution:
         elif root.val < p.val and root.val < q.val:
             return self.lowestCommonAncestor(root.right, p, q)
         else:
+            # 其他
             return root
+
+    def lowestCommonAncestor1(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+        非递归方法
+        """
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return  root
+        return root
